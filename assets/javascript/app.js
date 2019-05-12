42e6ddf83d5dd19bd15626d69cf5396bb0fb89c3
John Blackshear <swjaybee@gmail.com>
2019-05-11 21:51:11 -0500

    init

---
+++ assets/javascript/app.js
@@ -0,0 +1,101 @@
+$(document).ready(function(){
+
+var img = $('<img>');
+
+var questions = [
+    
+{   
+    question: "WHAT TEAM IS THIS?",
+    a:"Minnesota Vikings",
+    b:"Houston Texans",
+    c:"Seatle Seahawks",
+    d:"New York Giants",
+    name:"q1",
+    image: img.attr('src', "./assets/images/Houston-Texans.png"),
+
+    correct:"b",
+},
+{   
+    question: "WHAT TEAM IS THIS?",
+    a:"Minnesota Vikings",
+    b:"Houston Texans",
+    c:"Seatle Seahawks",
+    d:"New York Giants",
+    name:"q1",
+    image: img.attr('src', "./assets/images/Minnesota-Vikings.png"),
+
+    correct:"b",
+},
+
+{   
+    question: "WHAT TEAM IS THIS?",
+    a:"Minnesota Vikings",
+    b:"Houston Texans",
+    c:"Seatle Seahawks",
+    d:"New York Giants",
+    name:"q1",
+    image: img.attr('src', "./assets/images/New-England-Patriots.png"),
+
+    correct:"b",
+},
+
+];
+
+var generateQuestion = function(obj){
+
+	var form = $("<form>");
+		var question = $('<h2>').text(obj.question);
+		var aDiv = $('<div>');
+		var bDiv = $('<div>');
+		var cDiv = $('<div>');
+		var dDiv = $('<div>');
+		var a = $('<input type="radio">').attr('name', obj.name);
+		aDiv.append(a);
+		aDiv.append(obj.a);
+		var b = $('<input type="radio">').attr('name', obj.name);
+		bDiv.append(b);
+		bDiv.append(obj.b);
+		var c = $('<input type="radio">').attr('name', obj.name);
+		cDiv.append(c);
+		cDiv.append(obj.c);
+		var d = $('<input type="radio">').attr('name', obj.name);
+		dDiv.append(d);
+        dDiv.append(obj.d);
+        
+$('#questionbox').append(img);
+form.append(question);
+form.append(aDiv, bDiv, cDiv, dDiv);
+$('#answerbox').append(form);
+}
+
+generateQuestion(questions[0]);
+
+
+
+   
+    // code for image
+    /*var img = $('<img>');
+    img.attr('src', "./assets/images/Houston-Texans.png");
+    (img).append()
+    $('#questionbox').append(img);*/
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
