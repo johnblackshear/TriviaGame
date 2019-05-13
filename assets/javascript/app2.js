70a487b53b778d007997a4510a4bbcee50c5b8d5
John Blackshear <swjaybee@gmail.com>
2019-05-13 00:31:00 -0500

    init

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -2,13 +2,14 @@ $(document).ready(function (){
 
 var rigthAnswers = 0;
 var wrongAnswers = 0;
+var unaswered= 0;
 var timeRemaining = 20;
 var index = 0;
 var timerId = 0;
-var answered;
+var answered = false;
 var correct;
 
-var questions = [{
+var game = [{
     question:"What team is it?",
     answer:["Texas Oilers","Houston Sulleys","Houston Texans","Dallas Monstars"],
     correct:"2",
@@ -27,10 +28,34 @@ var questions = [{
 
 alert("you are linked")
 
-function start(){
+// function that starts the game
+function startTriva(){
     $('.start-button').remove();
     rigthAnswers = 0;
     wrongAnswers = 0;
+    unaswered = 0;
+    loadQuestions();
 }
+
+// function that sets timer 
+function loadQuestions(){
+    answered = false; 
+    timeRemaining = 20;
+    timerId = setInterval(timer, 1000);
+    if (answered === false){
+        timer();
+    }
+    correct = game[index].correct;
+    var image = game[index].image;
+    $('.imageDiv').html(image);
+    var question = game[index].question;
+    $('.question').html(question);
+}
+$('.startButton').on("click", function () {
+    $('.startButton');
+    startTriva();
+
+});
+
 
 });
