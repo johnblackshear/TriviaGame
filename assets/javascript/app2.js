3422e94a95ee36b40d852741a0d36d339dab1218
John Blackshear <swjaybee@gmail.com>
2019-05-13 20:49:05 -0500

    init

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -12,17 +12,17 @@ var correct;
 var game = [{
     question:"What team is it?",
     answer:["Texas Oilers","Houston Sulleys","Houston Texans","Dallas Monstars"],
-    correct:"2",
+    correct:"1",
     image: ("./assets/images/Houston-Texans.png")
 }, {
     question: "WHAT TEAM IS THIS?",
     answer: ["Minnesota Goofies","Minnesota Vikings","Minneapolis Goofies","Minneapolis Norsmen"],
-    correct: "1",
+    correct: "0",
     image: ("./assets/images/Minnesota-Vikings.png")
 },{
     question: "WHAT TEAM IS THIS?",
     answer: ["Boston Patriots"," Boston Hatsmen","New England Mad Hatters","New England Patriots"],
-    correct: "3",
+    correct: "2",
     image: ("./assets/images/New-England-Patriots.png")
 }];
 
@@ -46,11 +46,58 @@ function loadQuestions(){
         timer();
     }
     correct = game[index].correct;
-    var image = game[index].image;
-    $('.imageDiv').html(image);
+    $('.imageDiv').append('<img class=questionImage width="150" height"150 src="' + game[index].image + ' ">');
     var question = game[index].question;
     $('.question').html(question);
+    for(var i = 0; i < 4; i++){
+        var answer = game[index].answer[i];
+        $('.asnwers').append('<h4 class = answerAll id=' + i + '>')
+    }
 }
+
+$('h4').click(function(){
+    var id = $(this).attr('id');
+    if(id === correct){
+        answered === true;
+        $('.question').text("The answer is: " + game[index].answer[correct]);
+        rightAnswer();
+    }else{
+        $('.question').text("your choice was: " + game[index].answer[id] + " was wrong! The right answer is: " + game[index].answer[correct]);
+        wrongAnswer();
+    }
+    
+});
+
+function timer(){
+    if (timeRemaining === 0){
+        clearInterval(timerId);
+    }else{
+        timeRemaining--;
+        $('.timeRemaining').text('You have ' + timeRemaining + ' seconds');
+    }
+}
+
+function rightAnswer(){
+    rigthAnswers++;
+    $(".timeRemaining").text("Great Job! You have answered right!")
+    nextRound();
+}
+
+function unAnswer(){
+    unaswered++;
+    $('.timeRemaining').text("Why no choice?")
+    nextRound();
+}
+
+function wrongAnswer(){
+    wrongAnswers++;
+    $('timeRemaining').text("Better luck next time!");
+    nextRound();
+}
+
+
+
+
 $('.startButton').on("click", function () {
     $('.startButton');
     startTriva();
